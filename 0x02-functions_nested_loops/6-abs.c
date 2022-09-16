6684ebc68e0aac24cb05731921b245939d88717d
JudeTulel <judeyego202@gmail.com>
2022-09-15 20:33:50 -0700

    ok

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 /**
- *_abs - Chenck main.h
- * @r: An integre input
- * Description: This function returns absolute value of a number
- * Return: Absolut value of number r
-*/
+ * _abs - function that computes the absolute value of an integer
+ * @c:  is the int that will use for the argument of the function
+ * Return: 0
+ */
 int _abs(int c)
 {
-	if (c >= 0)
-		return (c);
+	if (c > 0 || c == 0)
+	{
+	return (c);
+	}
 	else
-		return (c * -1);
-} 
+	return (c * -1);
+}
